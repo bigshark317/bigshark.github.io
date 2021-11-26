9e1c14990766e132b62642bcc6640b6e947ef7a5
bigshark317 <1349263991@qq.com>
2021-11-26 19:33:19 +0800

    dynamic

--- js/Untitled-1.js
+++ js/Untitled-1.js
@@ -1,17 +1,9 @@
-//变换背景颜色
-/**
- * 产生随机整数，包含下限值，包括上限值
- * @param {Number} lower 下限
- * @param {Number} upper 上限
- * @return {Number} 返回在下限到上限之间的一个随机整数
- */
- function random(lower, upper) {
-	return Math.floor(Math.random() * (upper - lower + 1)) + lower;
+function getCurrentTime () {
+    let now = new Date ();
+    let hh = now.getHours();
+    let mm = now.getMinutes() > 10 ? now.getMinutes() : '0'+now.getMinutes();
+    let ss = now.getSeconds() > 10 ? now.getSeconds() : '0'+ now.getSeconds();  
+    var currentTime = hh + ':' + mm + ':' + ss;
+	document.getElementById("curt").innerHTML=currentTime;
 }
-//随机设置背景色
-function setBGC() {
-	var colorArr = new Array("#4395D1", "#00A99D", "#FBAF5D", "#84871C", "#9E6B52", "#A186BE");
-	var i = random(0, 5);
-	document.getElementById("imgBg").style.background = colorArr[i];
-}
-setInterval(setBGC, 300);
+setInterval(getCurrentTime, 1000)
